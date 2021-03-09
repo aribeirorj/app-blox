c53acfa5408a5c9824f08f796f6e394457e39c76
Andre Silva <3rd.andre.silva@amedigital.com>
2021-03-09 15:39:06 -0300

    chore: adicionado o styled component

---
+++ src/pages/main/index.js
@@ -0,0 +1,14 @@
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>My First React Component!</h1>
+      </div>
+    );
+  }
+}
+
+export default Main();
