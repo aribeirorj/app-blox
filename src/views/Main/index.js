2b7bb940a5b6ec8f2975560effd10477dcabd9f1
Andre Luiz <aribeiro.rj@gmail.com>
2021-03-13 22:18:28 -0300

    refact: yarn

--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { Container } from "./styles";
-import Header from "../../components/Header";
 import DashBoard from "./../Dashboard";
-import Footer from "../../components/Footer";
+import { Header, Footer } from "../../components";
 
 function Main() {
   return (
