28f4fa0157d3d35e5e4edfed12f12443323717e5
Andre Luiz <aribeiro.rj@gmail.com>
2021-03-13 22:07:04 -0300

    refactor: ajuste no styled

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -7,13 +7,6 @@ const GlobalStyle = createGlobalStyle`
 
   }
 
-  body {
-    margin: 0;
-    padding: 0;
-    outline: 0;
-    /* box-sizing: border-box */
-  }
-
   *:focus {
     outline:0;
   }
